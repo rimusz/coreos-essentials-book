ce4c8fdcc7d5a7dfeead3e4bebd5f93de86ff071
rimusz <rmocius@gmail.com>
2015-07-12 14:32:48 +0100

    no message

--- Chapter6/Test_Staging_Cluster/webserver/deploy_2_staging1.sh
+++ Chapter6/Test_Staging_Cluster/webserver/deploy_2_staging1.sh
@@ -10,10 +10,11 @@ project=$(cat ~/coreos-tsc-gce/settings | grep project= | head -1 | cut -f2 -d"=
 zone=$(cat ~/coreos-tsc-gce/settings | grep zone= | head -1 | cut -f2 -d"=")
 
 # change folder permissions
-gcloud compute --project=$project ssh  --zone=$zone "core@tsc-staging1" --command "sudo chmod -R 755 /home/core/share/"
+gcloud compute --project=$project ssh --zone=$zone "core@tsc-staging1" --command "sudo chmod -R 755 /home/core/share/"
+gcloud compute --project=$project ssh --zone=$zone "core@tsc-staging1" --command "sudo chown -R core /home/core/share/"
 
 echo "Deploying code to tsc-staging1 server !!!"
-gcloud compute copy-files staging1/index.html tsc-staging1:/home/core/share/nginx/html --zone $zone --project $project
+gcloud compute copy-files staging1/index.html core@tsc-staging1:/home/core/share/nginx/html --zone $zone --project $project
 
 echo " "
 echo "Finished !!!"
